b007ffa821a0ddc00aa58046eb57cff7c4ac0532
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-06-29 23:20:49 -0400

    fetch reviews working

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react"
 import "./LandingPage.css"
 import LikeButter from "../../assets/LikeButter.png"
 
 export default function LandingPage() {
+    const [reviews, setReviews] = useState([])
+    const [avg, setAvg] = useState()
 
+    
+    useEffect(() => {
+        const getReviews = async () => {
+            await fetch("https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJfYYmp7pJBgMRPft-XA0nNiA&key=AIzaSyAZ2y7dzznlJWMmWDzH9Nx9FEuQzrVupOk")
+            .then((res) => {return res.json()})
+            .then((res) => {
+                setReviews(res.result.reviews)
+                setAvg(res.result.rating)
+            })
+        }
+        getReviews()
+    }, [])
+
+    if (reviews.length) {
     return (
         <div className="content">
             <div className="main-img">
@@ -11,6 +28,12 @@ export default function LandingPage() {
             <div className="slogan">
                 simple food... elevated
             </div>
+            {/* <div className="reviews">
+                {reviews.map((review) => {
+                    return review.text
+                })}
+            </div> */}
         </div>
     )
+    }
 }
