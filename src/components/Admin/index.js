c3a2929ece9482b9a12744c902225423d6b073db
Eric Kinder <etkndr@gmail.com>
2024-01-17 01:20:17 -0500

    remove csrf fetch

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,19 +1,9 @@
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { baseUrl } from "../../store/actions"
+import { logout } from "../../store/session"
 import LoginForm from "./LoginForm"
 import * as menuActions from "../../store/menu"
-
-export const csrf = await fetch(`${baseUrl}/api/auth/user`, {
-  method: "GET",
-  mode: "cors",
-  credentials: "include", // includes cookies, authorization in request headers
-})
-  .then((res) => res.json())
-  .then((msg) => {
-    console.log(msg)
-    return `${msg.csrf_token}`
-  })
 
 export default function Admin() {
   const dispatch = useDispatch()
@@ -23,5 +13,10 @@ export default function Admin() {
     return <LoginForm />
   }
 
-  return <>{sessionUser.username}</>
+  return (
+    <>
+      {sessionUser.username}
+      <button onClick={() => dispatch(logout())}>log out</button>
+    </>
+  )
 }
