1a4853e58338e78c077161f4c40c1f4979de95f8
Eric Kinder <etkndr@gmail.com>
2024-02-09 16:43:14 -0500

    nav contact move

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -1,24 +1,23 @@
-import NavBar from "./NavBar";
+import NavBar from "./NavBar"
 import Contact from "./Contact"
 import lbSmall from "../../assets/LB-small.png"
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom"
 import "./Navigation.css"
-
 
 export default function Header() {
   return (
     <header>
+      <NavLink to="/" className="logo">
+        <div className="nav-logo">
+          <img src={lbSmall} alt="nav-logo" />
+        </div>
+      </NavLink>
       <div className="nav-area">
-        <NavLink to="/" className="logo">
-          <div className="nav-logo">
-            <img src={lbSmall} alt="nav-logo"/>
-          </div>
-        </NavLink>
         <NavBar />
-      </div>
-      <div className="nav-contact">
-        <Contact/>
+        <div className="nav-contact">
+          <Contact />
+        </div>
       </div>
     </header>
-  );
-};
+  )
+}
